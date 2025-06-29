ab2f523e6eca680931c8135103901770487ac297
Jasper Oosthoek <jasperoosthoek@hotmail.com>
2025-06-29 20:56:08 +0200

    #1 Get browser language to set language for localization

--- packages/demo/src/app/components/LanguageDropdown.tsx
+++ packages/demo/src/app/components/LanguageDropdown.tsx
@@ -1,10 +1,15 @@
 import { Dropdown } from 'react-bootstrap';
 import { useLocalization, useLocalStorage} from '@jasperoosthoek/react-toolbox';
+import { locales } from '../localization/localization';
 
 const LanguageDropdown = () => {
   const { setLanguage, languages, text, textByLang } = useLocalization();
+  // Get the base language from the browser's language setting
+  // This will be used to set the default language if it is available in the locales
+  const baseLanguage = navigator.language.split('-')[0];
 
-  const [, setLang] = useLocalStorage('lang', 'en');
+  // Use localStorage to persist the selected language
+  const [, setLang] = useLocalStorage('lang', !!locales[baseLanguage] ? baseLanguage : 'en');
   return (
     <Dropdown className='language-dropdown'>
       <Dropdown.Toggle variant="outline-primary">
